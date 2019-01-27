1d2bc9302324fcbc7b9dcdd9fc8335ea0edda014
Manish Tiwari <manish.t92@gmail.com>
2019-01-28 00:22:01 +0530

    Project Added

---
+++ AppCenterExample/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace AppCenterExample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+            AppCenter.Start("ios=db53fbdd-1b17-45b5-8b21-ce008cc53778;" + "uwp=04b94821-b30b-4219-b2e6-c5eb2a1325e6;" + "android=5c9910e1-d44b-4489-b283-0ac71fab54c5;", typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
