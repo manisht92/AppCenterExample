f26cf2866917c70d785d93ca2971c2e5b28dc1b4
Manish Tiwari <manish.t92@gmail.com>
2019-02-04 22:26:25 +0530

    ICommand Implemented

---
+++ AppCenterExample/MyViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace AppCenterExample
+{
+    public class MyViewModel : INotifyPropertyChanged
+    {
+        public int count = 0;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string DisplayCount => $"You clicked {count} times";
+
+        public MyViewModel()
+        {
+            IncreaseCommand = new Command(IncreaseCount);
+        }
+
+        public ICommand IncreaseCommand { get; }
+
+        void IncreaseCount()
+        {
+            count++;
+            OnPropertyChanged(nameof(DisplayCount));
+        }
+
+        public void OnPropertyChanged([CallerMemberName]string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+    }
+}
