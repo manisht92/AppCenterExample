3c985746167e2805e8a6aa9785fabe140c604ce6
Manish Tiwari <manish.t92@gmail.com>
2019-02-08 08:52:39 +0530

    Behavior

--- AppCenterExample/CustomBehaviorPage.xaml.cs
+++ AppCenterExample/CustomBehaviorPage.xaml.cs
@@ -1,15 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace AppCenterExample
 {
     public partial class CustomBehaviorPage : ContentPage
     {
+        public ICommand OutputAgeCommand { get; private set; }
+
         public CustomBehaviorPage()
         {
             InitializeComponent();
+
+            List<string> data = new List<string>
+            {
+                "a","b","c"
+            };
+            DemoList.ItemsSource = data;
+            OutputAgeCommand = new Command(OutputAge);
+        }
+
+        public void OutputAge()
+        {
+            int abc = 1;
         }
     }
 }
---
+++ AppCenterExample/ListViewSelectedItemBehavior.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace AppCenterExample
+{
+    public class ListViewSelectedItemBehavior : Behavior<ListView>
+    {
+        public static readonly BindableProperty CommandProperty =
+            BindableProperty.Create("Command", typeof(ICommand), typeof(ListViewSelectedItemBehavior), null);
+
+        public ListView MyListView { get; set; }
+
+        public ICommand Command
+        {
+            get { return (ICommand)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        protected override void OnAttachedTo(ListView bindable)
+        {
+            base.OnAttachedTo(bindable);
+            MyListView = bindable;
+            bindable.BindingContextChanged += OnBindingContextChanged;
+            bindable.ItemSelected += OnListViewItemSelected;
+        }
+
+        protected override void OnDetachingFrom(ListView bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.BindingContextChanged -= OnBindingContextChanged;
+            bindable.ItemSelected -= OnListViewItemSelected;
+            MyListView = null;
+        }
+
+        void OnBindingContextChanged(object sender, EventArgs e)
+        {
+            OnBindingContextChanged();
+        }
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+            BindingContext = MyListView.BindingContext;
+        }
+
+        void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (Command == null)
+            {
+                return;
+            }
+
+            //object parameter = Converter.Convert(e, typeof(object), null, null);
+            //if (Command.CanExecute(parameter))
+            //{
+            //    Command.Execute(parameter);
+            //}
+        }
+    }
+}
